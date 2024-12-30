365eb3c5381670966caedf39051686696b091e77
Melon <melon@thinkingsoftware.tw>
2024-12-30 11:24:46 +0800

    create AddEventViewModel and UI States

---
+++ app/src/main/java/com/example/shoppingevents/ui/addEvent/AddEventUiState.kt
@@ -0,0 +1,15 @@
+package com.example.shoppingevents.ui.addEvent
+
+data class AddEventDetails(
+    val id: Long = 0,
+    val name: String = "",
+    val initialBudget: Double = 0.0,
+    val totalCost: Double = 0.0,
+    val eventDate: String = "",
+    val completed: Boolean = false
+
+)
+data class AddEventUiState(
+    val addEventDetails: AddEventDetails = AddEventDetails(),
+    val isEntryValid: Boolean = false
+)
---
+++ app/src/main/java/com/example/shoppingevents/ui/addEvent/AddEventViewModel.kt
@@ -0,0 +1,30 @@
+package com.example.shoppingevents.ui.addEvent
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableDoubleStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class AddEventViewModel @Inject constructor(): ViewModel() {
+    var addEventUiState by mutableStateOf(AddEventUiState())
+
+    fun updateUiState(
+        addEventDetails: AddEventDetails
+    ){
+        addEventUiState = AddEventUiState(
+            addEventDetails = addEventDetails
+        )
+    }
+    private fun validateInput(
+        eventDetails: AddEventDetails = addEventUiState.addEventDetails
+    ): Boolean{
+        return with(eventDetails){
+            name.isNotBlank() && eventDate.isNotBlank()
+        }
+
+    }
+}

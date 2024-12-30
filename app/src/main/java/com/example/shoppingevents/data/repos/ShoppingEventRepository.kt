732fd3263a96cbe625f2ff2d26e94821f1508bb0
Melon <melon@thinkingsoftware.tw>
2024-12-30 15:47:02 +0800

    create repositories for event and item

--- app/src/main/java/com/example/shoppingevents/data/repos/ShoppingEventRepository.kt
+++ app/src/main/java/com/example/shoppingevents/data/repos/ShoppingEventRepository.kt
@@ -1,4 +1,34 @@
 package com.example.shoppingevents.data.repos
 
-class ShoppingEventRepository {
+import com.example.shoppingevents.data.daos.ShoppingEventDao
+import com.example.shoppingevents.data.entities.ShoppingEvent
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+interface ShoppingEventRepository{
+    suspend fun insert(shoppingEvent: ShoppingEvent)
+    suspend fun update(shoppingEvent: ShoppingEvent)
+    suspend fun delete(shoppingEvent: ShoppingEvent)
+    fun getEvents(): Flow<List<ShoppingEvent>>
+}
+
+class ShoppingEventRepositoryImpl @Inject constructor(
+    private val shoppingEventDao: ShoppingEventDao
+): ShoppingEventRepository{
+    override suspend fun insert(shoppingEvent: ShoppingEvent) {
+        shoppingEventDao.insert(shoppingEvent)
+    }
+
+    override suspend fun update(shoppingEvent: ShoppingEvent) {
+        shoppingEventDao.update(shoppingEvent)
+    }
+
+    override suspend fun delete(shoppingEvent: ShoppingEvent) {
+        shoppingEventDao.delete(shoppingEvent)
+    }
+
+    override fun getEvents(): Flow<List<ShoppingEvent>> {
+        return shoppingEventDao.getEvents()
+    }
+
 }

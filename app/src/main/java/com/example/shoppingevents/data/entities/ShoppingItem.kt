5e8624e86d504054b8a9d83b7f7b5b54e849d9e4
Melon <melon@thinkingsoftware.tw>
2024-12-30 15:09:32 +0800

    Define Entities for Database

--- app/src/main/java/com/example/shoppingevents/data/entities/ShoppingItem.kt
+++ app/src/main/java/com/example/shoppingevents/data/entities/ShoppingItem.kt
@@ -1,9 +1,21 @@
 package com.example.shoppingevents.data.entities
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity (tableName = "shopping_item")
 data class ShoppingItem(
+    @PrimaryKey(autoGenerate = true)
     val itemId: Long = 0,
+
+    @ColumnInfo(name = "event_id")
     val eventId: Long,
+
+    @ColumnInfo(name = "item_name")
     val itemName: String,
+
     val price: Double = 0.0,
+
     val quantity: Int = 0,
 )
